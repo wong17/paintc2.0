be1baf765deab06a4be239d3b3b907c526b7b5a2
Wong <josuewong797@gmail.com>
2024-01-27 21:59:07 -0600

    Se corrigio el funcionamiento del zoom

--- Paintc2.0/Paintc/Controller/UserControls/DrawingPanelController.cs
+++ Paintc2.0/Paintc/Controller/UserControls/DrawingPanelController.cs
@@ -10,10 +10,7 @@ namespace Paintc.Controller.UserControls
         private readonly DrawingPanel _DrawingPanel;
 
         // Crear matrix que va a contener la transformación a aplicar
-        private readonly MatrixTransform matrixTransform = new();
-
-        private bool isSpacebarPressed = false;
-        private Point lastMousePosition;
+        private readonly ScaleTransform scaleTransform = new();
 
         public DrawingPanelController(DrawingPanel drawingPanel)
         {
@@ -23,73 +20,26 @@ namespace Paintc.Controller.UserControls
 
         private void InitController()
         {
-            _DrawingPanel.MainScrollViewer.PreviewMouseWheel += ScrollViewer_PreviewMouseWheel;
-            _DrawingPanel.MainScrollViewer.PreviewMouseMove += ScrollViewer_PreviewMouseMove;
-            _DrawingPanel.MainScrollViewer.PreviewKeyDown += ScrollViewer_PreviewKeyDown;
-            _DrawingPanel.MainScrollViewer.PreviewKeyUp += ScrollViewer_PreviewKeyUp;
+            _DrawingPanel.CustomCanvas.LayoutTransform = scaleTransform;
+            _DrawingPanel.CustomCanvas.MouseWheel += CustomCanvas_MouseWheel;
         }
 
-        private void ScrollViewer_PreviewMouseWheel(object sender, MouseWheelEventArgs e)
+        private void CustomCanvas_MouseWheel(object sender, MouseWheelEventArgs e)
         {
-            if (Keyboard.Modifiers != ModifierKeys.Control)
-            {
-                e.Handled = true;
-                return;
-            }
+            if (Keyboard.Modifiers != ModifierKeys.Control) return;
+
             // Positivo si se gira hacia adelante (hacia el monitor), Negativo si se gira hacia atras (hacia el usuario)
-            var factor = (e.Delta > 0) ? (1.09) : (1 / 1.09);
-            // Obtener posición del mouse
+            var factor = (e.Delta > 0) ? (0.1) : (-0.1);
+            // Obtener posición del mouse sobre el canvas
             Point mousePosition = e.GetPosition(_DrawingPanel.CustomCanvas);
-            // Crear matriz de escalación asignando la matriz de transformación actual (anterior)
-            Matrix scaleMatrix = matrixTransform.Matrix;
-            // Escalar centrado en la posición actual del mouse
-            scaleMatrix.ScaleAt(factor, factor, mousePosition.X, mousePosition.Y);
-            // Asignar la nueva matriz escalada a la matriz de transformación
-            matrixTransform.Matrix = scaleMatrix;
-
-            // Aplica la transformación de escala al Canvas
-            _DrawingPanel.CustomCanvas.LayoutTransform = matrixTransform;
-
-            // Ajusta las barras de desplazamiento del ScrollViewer
-            _DrawingPanel.MainScrollViewer.ScrollToHorizontalOffset(_DrawingPanel.MainScrollViewer.HorizontalOffset * factor);
-            _DrawingPanel.MainScrollViewer.ScrollToVerticalOffset(_DrawingPanel.MainScrollViewer.VerticalOffset * factor);
-
-            // Indica que el evento ha sido manejado
-            e.Handled = true;
-        }
-
-        private void ScrollViewer_PreviewMouseMove(object sender, MouseEventArgs e)
-        {
-            if (isSpacebarPressed)
-            {
-                // Obtener nueva posición del mouse
-                Point newMousePosition = e.GetPosition(_DrawingPanel.CustomCanvas);
-                // Calcular diferencia en el cambio de posición
-                Vector delta = Point.Subtract(newMousePosition, lastMousePosition);
-                // Ajustar scroll
-                _DrawingPanel.MainScrollViewer.ScrollToHorizontalOffset(_DrawingPanel.MainScrollViewer.HorizontalOffset - delta.X);
-                _DrawingPanel.MainScrollViewer.ScrollToVerticalOffset(_DrawingPanel.MainScrollViewer.VerticalOffset - delta.Y);
-
-                // Actualiza ultima posición
-                lastMousePosition = newMousePosition;
-            }
-        }
-
-        private void ScrollViewer_PreviewKeyDown(object sender, KeyEventArgs e)
-        {
-            if (e.Key == Key.Space)
-            {
-                lastMousePosition = Mouse.GetPosition(_DrawingPanel.CustomCanvas);
-                isSpacebarPressed = true;
-            }
-        }
-
-        private void ScrollViewer_PreviewKeyUp(object sender, KeyEventArgs e)
-        {
-            if (e.Key == Key.Space)
-            {
-                isSpacebarPressed = false;
-            }
+            // Aplicar escalación centrada sobre la posición actual del mouse
+            scaleTransform.CenterX = mousePosition.X;
+            scaleTransform.CenterY = mousePosition.Y;
+            scaleTransform.ScaleX = Math.Max(1.0, scaleTransform.ScaleX + factor);
+            scaleTransform.ScaleY = Math.Max(1.0, scaleTransform.ScaleY + factor);
+            // Ajustar las barras de desplazamiento del ScrollViewer
+            _DrawingPanel.MainScrollViewer.ScrollToHorizontalOffset(_DrawingPanel.MainScrollViewer.HorizontalOffset + mousePosition.X * factor);
+            _DrawingPanel.MainScrollViewer.ScrollToVerticalOffset(_DrawingPanel.MainScrollViewer.VerticalOffset + mousePosition.Y * factor);
         }
     }
 }
