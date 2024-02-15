ed947a5e345732988e3014b3d19464bb0d301862
Wong <josuewong797@gmail.com>
2024-02-14 19:41:43 -0600

    Se agrego DetailPanel que contendra toda la información sobre cada forma que se vaya agregando al canvas

---
+++ Paintc2.0/Paintc/View/UserControls/DetailPanel.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.Windows.Controls;
+
+namespace Paintc.View.UserControls
+{
+    /// <summary>
+    /// Interaction logic for DetailPanel.xaml
+    /// </summary>
+    public partial class DetailPanel : UserControl
+    {
+        public DetailPanel()
+        {
+            InitializeComponent();
+        }
+    }
+}
