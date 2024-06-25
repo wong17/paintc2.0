a49568d71fe2d30eb89e9af98a512806b71d879f
Wong <josuewong797@gmail.com>
2024-06-24 19:58:24 -0600

    Se cambia versión de 1.0 a 1.1 y se agrega lógica para abrir enlace del repositorio de github

--- Paintc2.0/Paintc/ViewModels/AboutWindowViewModel.cs
+++ Paintc2.0/Paintc/ViewModels/AboutWindowViewModel.cs
@@ -1,8 +1,32 @@
-﻿using Paintc.Core;
+﻿using Paintc.Commands;
+using Paintc.Core;
+using System.Diagnostics;
+using System.Windows;
 
 namespace Paintc.ViewModels
 {
-    public class AboutWindowViewModel() : ViewModel
+    public class AboutWindowViewModel : ViewModel
     {
+        public RelayCommand OpenGithubRepositoryCommand { get; private set; }
+
+        public AboutWindowViewModel()
+        {
+            OpenGithubRepositoryCommand = new RelayCommand(obj => true, OpenGithubRepository);
+        }
+
+        private void OpenGithubRepository(object? uri)
+        {
+            if (uri is null)
+                return;
+
+            try
+            {
+                Process.Start(new ProcessStartInfo((string)uri) { UseShellExecute = true });
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, "Error", MessageBoxButton.OK, MessageBoxImage.Error);
+            }
+        }
     }
 }
